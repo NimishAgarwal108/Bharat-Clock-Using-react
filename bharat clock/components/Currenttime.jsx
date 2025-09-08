8efe9b5c9fad19b9f5d8f60be1f5d88238f15c6c
Nimish Agarwal <agarwalnimish428@gmail.com>
2025-09-08 16:18:32 +0530

    updated

--- bharat clock/components/Currenttime.jsx
+++ bharat clock/components/Currenttime.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
 function Currenttime(){
    let Current_Date = new Date();
-   let time= new Date();
-  let day= new Date();
+   let day= new Date();
+
+
+  const[time,setTime]=useState(new Date());
+
+  useEffect(()=>{
+    const intervalId=setInterval(()=>{
+      setTime(new Date())
+    },1000);
+    return()=>{
+      clearInterval(intervalId);
+              }
+
+  },[]);
+
+
+
   return(
     <div className="Currenttime">
       <p>this is the current time:{Current_Date.toLocaleDateString()}-{time.toLocaleTimeString()}-{day.toLocaleDateString('en-US', { weekday: 'long' })}
